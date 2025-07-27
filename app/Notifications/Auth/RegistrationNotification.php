e5f64db83346524cf2e228383615281d2e893953
SailendraRawal <abhishekthapa115@gmail.com>
2025-07-27 11:23:44 +0545

    remove queue for now

--- app/Notifications/Auth/RegistrationNotification.php
+++ app/Notifications/Auth/RegistrationNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class RegistrationNotification extends Notification implements ShouldQueue
+class RegistrationNotification extends Notification
 {
     use Queueable;
 
